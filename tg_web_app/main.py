858820af85121b4e5bc1824905f016c14f00e661
Клим <klim.klushin@gmail.com>
2025-08-23 14:28:28 +0300

    Web Apps. Полноценные веб приложения в Телеграм

--- tg_web_app/main.py
+++ tg_web_app/main.py
@@ -1,5 +1,5 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
 from aiogram.types.web_app_info import WebAppInfo
 from settings import settings
@@ -15,6 +15,10 @@ async def start(message: types.Message):
     )
     markup = types.InlineKeyboardMarkup(inline_keyboard=[[hello_button]])
     await message.reply("Hello", reply_markup=markup)
+
+@dp.message(F.web_app_data)
+async def web_app(message: types.Message):
+    await bot.send_message(message.chat.id, f"Данные получены: {message.web_app_data.data}")
 
 if __name__ == "__main__":
     asyncio.run(dp.start_polling(bot))
