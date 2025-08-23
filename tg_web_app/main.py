c9f3b909e5774779dc4d7c0becd406f2ff86fd70
Клим <klim.klushin@gmail.com>
2025-08-23 15:20:23 +0300

    Web Apps. Полноценные веб приложения в Телеграм

--- tg_web_app/main.py
+++ tg_web_app/main.py
@@ -9,16 +9,22 @@ dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def start(message: types.Message):
-    hello_button = types.InlineKeyboardButton(
-        text="Open app",
-        web_app=WebAppInfo(url="https://neonavt.github.io/neonavt_tg_bot/")
+    kb = types.InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                types.InlineKeyboardButton(
+                    text="Открыть",
+                    web_app=WebAppInfo(url="https://neonavt.github.io/neonavt_tg_bot/")
+                )
+            ]
+        ]
     )
-    markup = types.InlineKeyboardMarkup(inline_keyboard=[[hello_button]])
-    await message.reply("Hello", reply_markup=markup)
+    await message.answer("Жми кнопку", reply_markup=kb)
 
-@dp.message(F.web_app_data)
-async def web_app(message: types.Message):
-    await bot.send_message(message.chat.id, f"Данные получены: {message.web_app_data.data}")
+@dp.message()
+async def get_data(message: types.Message):
+    if message.web_app_data:
+        await message.answer(f"Данные получены: {message.web_app_data.data}")
 
 if __name__ == "__main__":
     asyncio.run(dp.start_polling(bot))
